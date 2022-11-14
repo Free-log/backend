d82a86cf54073367476b96ef43052f584a54e725
wooseokchoe <chldntjr0425@gmail.com>
2022-11-15 02:29:50 +0900

    build: initialize project with Spring Initializer

---
+++ src/main/java/com/techeer3rd/freelogbackend/FreeLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techeer3rd.freelogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeLogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeLogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techeer3rd/freelogbackend/FreeLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer3rd.freelogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeLogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
